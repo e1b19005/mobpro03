7b3a7888458b15d4dd780a93a7e616335925fbb7
e1b19005 <e1b19005@oit.ac.jp>
2021-10-12 14:21:42 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/lec03/kaizi/mobpro03/Mobpro03Application.java
@@ -0,0 +1,13 @@
+package oit.is.lec03.kaizi.mobpro03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mobpro03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mobpro03Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec03/kaizi/mobpro03/Mobpro03ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec03.kaizi.mobpro03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mobpro03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
